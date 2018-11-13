be731bf0a8a56fc2d9ba5443767c24f1318444e6
APNikitin <saxaul@mail.ru>
2018-11-14 00:03:59 +0500

    Нажимаем на кнопку и удаляем пункт меню. Вроде заработало

--- Lesson1/js/engine.js
+++ Lesson1/js/engine.js
@@ -12,10 +12,12 @@ Container.prototype.render = function()
 
 Container.prototype.remove = function()
 {
-     /*document.getElementById(this.id).parentElement.removeChild(document.getElementById(this.id));*/
-   console.log(this);
-    console.log("wwwww");
-    delete this;
+    console.log(this);
+     document.getElementById(this.id).remove();
+    /*document.getElementById(this.id).parentElement.removeChild(document.getElementById(this.id));*/
+   
+   /* console.log("wwwww");
+    delete this;*/
    /* if (document.getElementById(this.id)){
         console.log(document.getElementById(this.id));
     }
@@ -50,26 +52,27 @@ Menu.prototype.render = function(){
 	return result;
 }
 
-function MenuItem(my_href, my_name){
+function MenuItem(my_href, my_name,my_id){
    Container.call(this);
    this.className = "menu-item";
    this.href = my_href;
    this.itemName = my_name;
+   this.id = my_id; 
 }
 
 MenuItem.prototype = Object.create(Container.prototype);
 MenuItem.prototype.constructor = MenuItem;
 
 MenuItem.prototype.render = function(){
-	return "<li class='"+this.className+"' href='"+ this.href +"'>" + this.itemName + "</li>";
+	return "<li class='"+this.className+"' id='"+this.id+"' href='"+ this.href +"'>" + this.itemName + "</li>";
 }
 
-var m_item1 = new MenuItem("/", "Главная");
-var m_item2 = new MenuItem("/catalogue/", "Каталог");
-var m_item3 = new MenuItem("/gallery/", "Галерея");
+var m_item1 = new MenuItem("/", "Главная","general");
+var m_item2 = new MenuItem("/catalogue/", "Каталог","catalog");
+var m_item3 = new MenuItem("/gallery/", "Галерея","gallery");
 var m_items = {0: m_item1, 1: m_item2, 2: m_item3};
 
 var menu = new Menu("my_menu", "My_class", m_items);
 var div = document.write(menu.render());
-/*setTimeout(m_item3.remove(),1000);*/
-m_item1.remove();
+
+
