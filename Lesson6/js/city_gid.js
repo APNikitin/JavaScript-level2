a10b3522733168df50df7a3af571ef4c6c78d8e6
APNikitin <saxaul@mail.ru>
2018-12-03 00:42:44 +0500

    выбор города с json запросом с сервера

--- Lesson6/js/city_gid.js
+++ Lesson6/js/city_gid.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
+  
+    function CityList(){
+        
+    }
+    CityList.prototype.constructor = CityList;
     
-   /* $.getJson('city.json',function(data){
-        var City=[];
-        
-        $.each(data,function(item,subItem){
-           $('#r_city_list').append('<option/>')
-           
-        })
-        
-    })*/
-    var data=[];
-    $.get('city.json',function(data){
-        console.log(data);
-     /*data.forEach(function(item){
-         var option = $('#r_city_list').append('<option>');
-         option.value = item;
-     })*/
+    $.getJSON('city.json',function(CityList){
+      /*$.parseJSON(CityList);*/
+        /*console.log(CityList.city);*/
+        var option = '';
+     CityList.city.forEach(function(item){
+         option += '<option value='+item+'>';
+         
+         
+     })
+        $('#r_city_list').html(option);
     });
     
     
