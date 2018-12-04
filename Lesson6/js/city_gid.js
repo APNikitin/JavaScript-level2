eee23abe3fe8a5e07140dc5d8ea768dd0a6b5f01
APNikitin <saxaul@mail.ru>
2018-12-05 01:00:59 +0500

    Проверка на соответствие требованием полей ввеженного текста (поля: имя, телефон, e-mail)

--- Lesson6/js/city_gid.js
+++ Lesson6/js/city_gid.js
@@ -16,6 +16,40 @@ $(document).ready(function(){
      })
         $('#r_city_list').html(option);
     });
+   //____________click button send_________________________  
+    $('#btn_submit').click(function(){
+        //______________________verify name_________
+        var regexp = /^[a-zа-яё]+\s[a-zа-яё]+$/siu; //verify name
+       if (!regexp.test($('#r_name').val()) ){
+           $('#r_name').addClass('error_field');
+           alert($('#r_name').val() + regexp.test($('#r_name').val()));
+       }
+        else{
+            $('#r_name').removeClass('error_field');
+            alert($('#r_name').val() + regexp.test($('#r_name').val()));
+        };
+       //______________________verify email_________ 
+        regexp = /^\S+@\S+\.\S+$/;//verify email
+        if (!regexp.test($('#r_email').val()) ){
+           $('#r_email').addClass('error_field');
+           alert($('#r_email').val() + regexp.test($('#r_email').val()));
+       }
+        else{
+            $('#r_email').removeClass('error_field');
+            alert($('#r_email').val() + regexp.test($('#r_email').val()));
+        };
+       //______________________verify phone_________  
+        regexp = /^((8|\+\d)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        if (!regexp.test($('#r_tel').val()) ){
+           $('#r_tel').addClass('error_field');
+           alert($('#r_tel').val() + regexp.test($('#r_tel').val()));
+       }
+        else{
+            $('#r_tel').removeClass('error_field');
+            alert($('#r_tel').val() + regexp.test($('#r_tel').val()));
+        };
+        
+    });
     
     
 });
